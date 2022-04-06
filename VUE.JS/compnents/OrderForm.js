bc7ae0813b371f873f2c5acfc81612594191ca6c
siamsheikh1112 <mdsiamsheikh6640@gmail.com>
2022-04-06 16:12:49 +0600

    done

--- VUE.JS/compnents/OrderForm.js
+++ VUE.JS/compnents/OrderForm.js
@@ -14,47 +14,47 @@ app.component("order-form", {
   template:
     /*html*/
     `<div id="modal">
-    <div class="backdrop" @click="hideOrderForm"></div>
-    <div class="modalBody">
-    <div class="formContainer" id="orderForm">
-        <h1>Complete the form below and hit submit</h1>
-        <form class="orderForm" @submit.prevent="onSubmit">
-        <ul>
-            <li>
-            <input
-                type="text"
-                v-model="name"
-                class="fieldStyle fieldSplit alignLeft"
-                placeholder="Name"
-            />
-            <input
-                type="text"
-                v-model="phone"
-                class="fieldStyle fieldSplit alignRight"
-                placeholder="Phone no."
-            />  
-            </li>
-            <input
-                type="hidden"
-                v-model.number="total"
-                class="fieldStyle fieldSplit alignRight"
-            />
-            </li>
-            <li>
-            <textarea
-                v-model="address"
-                class="fieldStyle"
-                placeholder="Address"
-            ></textarea>
-            </li>
-            <li>
-            <input type="submit" value="Proceed" />
-            </li>
-        </ul>
-        </form>
-    </div>
-    </div>
-</div>`,
+      <div class="backdrop" @click="hideOrderForm"></div>
+      <div class="modalBody">
+      <div class="formContainer" id="orderForm">
+          <h1>Complete the form below and hit submit</h1>
+          <form class="orderForm" @submit.prevent="onSubmit">
+          <ul>
+              <li>
+              <input
+                  type="text"
+                  v-model="name"
+                  class="fieldStyle fieldSplit alignLeft"
+                  placeholder="Name"
+              />
+              <input
+                  type="text"
+                  v-model="phone"
+                  class="fieldStyle fieldSplit alignRight"
+                  placeholder="Phone no."
+              />  
+              </li>
+              <input
+                  type="hidden"
+                  v-model.number="total"
+                  class="fieldStyle fieldSplit alignRight"
+              />
+              </li>
+              <li>
+              <textarea
+                  v-model="address"
+                  class="fieldStyle"
+                  placeholder="Address"
+              ></textarea>
+              </li>
+              <li>
+              <input type="submit" value="Submit" /> 
+              </li>
+          </ul>
+          </form>
+      </div>
+      </div>
+  </div>`,
   data() {
     return {
       name: "",
@@ -65,6 +65,16 @@ app.component("order-form", {
   methods: {
     hideOrderForm() {
       this.$emit("toggle-order-form");
+    },
+    onsubmit() {
+      let orderDetails = {
+        name: this.name,
+        phone: this.phone,
+        address: this.address,
+        total: this.total,
+      };
+      console.log(orderDetails);
+      this.hideOrderForm();
     },
   },
 });
